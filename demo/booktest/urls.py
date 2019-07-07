020c252bf0a0564feb766e2816d584f77251d7af
4innocent <gong_kangjia@163.com>
2019-07-07 15:20:54 +0800

    Django-demo

--- demo/booktest/urls.py
+++ demo/booktest/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from booktest import views
 
-
+# 在urlpatterns中添加路由匹配规则
 urlpatterns = [
-    url(r'^index(/?)$', views.index)
+    # url的正则中()表达式中的内容会作为参数传入views的函数中
+    url(r'^index(/?)$', views.index),
+    url(r'^books/$', views.books_re),
+    url(r'^books/(\d+)$', views.detail)
 ]
--- demo/booktest/views.py
+++ demo/booktest/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from booktest.models import BookInfo
 
 # Create your views here.
 # 1.定义视图函数处理url请求
 # 2.在项目的urls中引入app的urls，并配置正则项
-# http://127.0.0.1:8080/index ----对应的即为index函数
+# http://127.0.0.1:8080/index
 
 # 必须传入HTTP请求
 def index(request,e):
@@ -19,3 +20,16 @@ def index(request,e):
 
     # 使用render函数替换模板中的变量,第一个变量是请求，第二个是模板的地址，第三个是数据
     return render(request,"booktest/index.html",grams)
+
+
+def books_re(request):
+    # 所有的图书信息
+    books = BookInfo.objects.all()
+
+    return render(request,"booktest/books.html",{"books":books})
+
+def detail(request,bid):
+    book = BookInfo.objects.get(id=bid)
+    heros = book.heroinfo_set.all()
+
+    return render(request, "booktest/detail.html", {"book":book,"heros":heros})
